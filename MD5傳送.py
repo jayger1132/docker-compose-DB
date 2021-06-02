7c75f50e29cbf6248ac5cdf33935bd16e50cc37b
jayger1132 <66061564+jayger1132@users.noreply.github.com>
2021-06-02 22:42:08 +0800

    完成

--- MD5傳送.py
+++ MD5傳送.py
@@ -2,7 +2,9 @@ import random
 import hashlib as hb 
 from pyDes import des, CBC, PAD_PKCS5 # des
 import base64
+import json
 
+path = 'D:\Gitinit\docker-compose-DB\MD5test.txt'#文檔路徑
 KEY='U0724023' # key 通常是 8bytes for DES 16/24 for triple 
 
 chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789' #random範圍
@@ -24,9 +26,13 @@ for i in range(1000):
 print ( str,"\n\nAlpha upper",alphaup,"| Alpha lower",alphalow,"| Number" ,num,"\n")
 
 # 訊息摘要 md5 產生 128bits 0/1 會以 32bits個hex顯示
-MD=hb.md5(str.encode("utf-8"))
-MD=MD.hexdigest() ## 訊息摘要
+MD=(hb.md5(str.encode("utf-8"))).hexdigest()# 訊息摘要
 print ("訊息摘要 (md5)",MD)
+'''
+binary 的方式 =>先從 16轉成 integer 再轉bin
+ad = int(MD,16)
+print (bin(ad))
+'''
 
 #github
 #https://github.com/twhiteman/pyDes
@@ -42,14 +48,24 @@ mode 為 CBC時候 需要有個IV向量
 '''
 iv = "".join(random.sample('0123456789',8))#list >> str 用join
 k = des(KEY , CBC , iv , pad=None , padmode=PAD_PKCS5)
-d = k.encrypt(MD)
-print ("訊息摘要加密(DES): %r" % base64.b64encode(d))
-#print ("Decrypted: %r" % k.decrypt(d))
+#加密 出來是 object的樣式
+en = k.encrypt(MD)
+
+ba = (base64.b64encode(en).decode("utf-8"))
+print ("訊息摘要加密(DES): %r" % ba)
+#print ("Decrypted: %r" % k.decrypt(en))
 
 '''
-要消除b '' 只需要decode
-de= k.decrypt(d)
+#要消除b '' 只需要decode
+de= k.decrypt(en)
 de =de.decode("utf-8")
 print(de)
 '''
 
+#https://jenifers001d.github.io/2019/12/11/Python/learning-Python-day9/
+#寫成json檔 比較好操作
+lines = {"str":str,'MD5':MD,'DES':ba,'KEY':KEY,'iv':iv}
+#如果會忘記 close 可以改用 with as
+with open(path,'w') as f:
+    json.dump(lines,f)
+    
